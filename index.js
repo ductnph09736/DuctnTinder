ea79cf3e6e77945fae2b87218e01acc72f8557c5
ductn <ngocduc0607@gmail.com>
2021-04-01 12:33:02 +0700

    aa

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ app.use(express.static('client'));
 server.listen(PORT, function (){
     console.log('NgocDuc is Running');
 })
+// var http =require('http');
+//
+// http.createServer(function (req, res){
+//     res.end('xin chao');
+// }).listen(process.env.port || 1106);
